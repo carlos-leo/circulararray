55cb2ebc6ab8abfef4007283167d75addee6a1b0
Carlos Enrique Leonardo Julian <55888437+carlos-leo@users.noreply.github.com>
2023-04-03 18:27:12 -0500

    Primer avance de tarea Formulación de algunos métodos: is_empty, isfull, size, clear, pop_back, pop_front

--- circulararray.h
+++ circulararray.h
@@ -39,3 +39,84 @@ CircularArray<T>::CircularArray(int _capacity)
     this->array = new T[_capacity];
     this->front = this->back = -1;//empty
 }
+
+template <class T>
+void CircularArray<T>::clear()
+{
+    this->front = this->back = -1;//empty
+}
+
+template <class T>
+bool CircularArray<T>::is_empty()
+{
+    if (this->front=-1 && this->back=-1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+template <class T>
+int CircularArray<T>::size()
+{
+    if(this->is_empty=true){
+        return 0;
+    }
+    else{
+        if(this->back>=this->front){
+            return (this->back + 1 - this->front);
+        }
+        else{
+            return (this->capacity - this->front + this->back + 1);;
+        }
+    }
+}
+
+template <class T>
+bool CircularArray<T>::is_full()
+{
+    if(this->capacity==this->size){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+template <class T>
+T CircularArray<T>::pop_back()
+{
+    if(this->is_empty=true){
+        return 0;
+    }
+    else{
+            this->back=this->back-1;
+            this->size=this->size-1;
+    }
+}
+
+template <class T>
+T CircularArray<T>::pop_front()
+{
+    if(this->is_empty=true){
+        return 0;
+    }
+    else{
+            this->front=this->front-1;
+            this->size=this->size-1;
+    }
+}
+
+template <class T>
+void CircularArray<T>::sort()
+{
+
+}
+
+template <class T>
+bool CircularArray<T>::is_sorted()
+{
+
+}
